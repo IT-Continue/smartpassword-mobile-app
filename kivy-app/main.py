1013a3e1ce21af4e3af28ac1793be24ad5a8645c
krons711 <vladk.711@gmail.com>
2023-10-31 02:10:40 +0300

    kivy setup#1

---
+++ kivy-app/main.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+
+class ScrollView1(ScrollView):
+    pass
+
+class BoxLayout1(BoxLayout):
+    pass
+
+class MainWidget(Widget):
+    pass
+
+class TheApp(App):
+    pass
+
+TheApp().run()
